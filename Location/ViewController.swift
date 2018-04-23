43573a71fd523c86d08fbae747785f4b8baa83fb
andrewtittle <atittle223@gmail.com>
2018-04-22 21:02:26 -0400

    initial commit

---
+++ Location/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Location
+//
+//  Created by Andrew Tittle on 4/22/18.
+//  Copyright © 2018 Andrew Tittle. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        let center = location.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
+    }
+
+}
+
